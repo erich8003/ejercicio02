e180c011fbc0f94b9d178189d933462194f8d15b
erich8003 <erich1980@gmail.com>
2023-05-03 11:50:00 -0400

    version inicial vacia

---
+++ src/test/java/cl/coopeuch/ejercicio02/Ejercicio02ApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.coopeuch.ejercicio02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejercicio02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
